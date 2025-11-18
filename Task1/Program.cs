3836b139eb9cad90200a7ea36724d6351589ae48
Aalxey <infurnus555@gmail.com>
2025-11-18 11:38:22 +0545

    Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student std = new Student();
+            std.Name = "Ram";
+            Console.WriteLine(std.Name);
+
+            Student stud = new Student();
+            stud.Name = "Hari";
+            Console.WriteLine(stud.Name);
+
+            Student.marks = 40;
+            Console.WriteLine(Student.marks);
+        }
+    }
+}
---
+++ Task1/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal class Student
+    {
+        public string Name;
+        public int Id;
+        public int Age;
+
+        public static int marks;
+    }
+}
