c19fe97f266c8a2bc02906ebeb68c212cf97d52f
Aalxey <infurnus555@gmail.com>
2025-11-19 09:54:38 +0545

    Task4

---
+++ Task4/Player.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    internal class Player
+    {
+        public string playerName;
+
+        public int level;
+
+        public String health;
+
+        public Player()
+        {
+            Console.WriteLine("Player has been called");
+        }
+
+        public Player(string playerName, int level, string health)
+        {
+            this.playerName = playerName;
+            this.level = level;
+            this.health = health;
+        }
+    }
+}
---
+++ Task4/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Task4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Player p1 = new Player();
+
+            Console.WriteLine("Player 1 Details:");
+            Console.WriteLine("Name: " + p1.playerName);
+            Console.WriteLine("Level: " + p1.level);
+            Console.WriteLine("Health: " + p1.health);
+            Console.WriteLine();
+
+            Player p2 = new Player("Bishal Rai", 10, "95%");
+
+            Console.WriteLine("Player 2 Details:");
+            Console.WriteLine("Name: " + p2.playerName);
+            Console.WriteLine("Level: " + p2.level);
+            Console.WriteLine("Health: " + p2.health);
+        }
+    }
+}
