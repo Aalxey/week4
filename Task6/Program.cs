e0fc36f8d3ebe3d04fd768b22bc6ec38983e72d7
Aalxey <infurnus555@gmail.com>
2025-11-19 12:04:19 +0545

    Task6

---
+++ Task6/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Task6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter marks obtained: ");
+            int marks;
+            while (!int.TryParse(Console.ReadLine(), out marks))
+            {
+                Console.WriteLine("Invalid input! Enter a valid integer.");
+            }
+
+            Console.Write("Enter total marks: ");
+            int total;
+            while (!int.TryParse(Console.ReadLine(), out total))
+            {
+                Console.WriteLine("Invalid input! Enter a valid integer.");
+            }
+
+            double percentage = marks / total * 100; 
+
+            Console.WriteLine("Percentage: " + percentage + "%");
+
+        }
+    }
+}
