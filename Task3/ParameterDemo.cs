e711fd7c600d0dd51adc1a43a9875f32d0927762
Aalxey <infurnus555@gmail.com>
2025-11-19 09:33:02 +0545

    Task3

---
+++ Task3/ParameterDemo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Task3
+{
+    internal class ParameterDemo
+    {
+        public int increase(ref int number)
+        {
+            number +=10;
+            return number;
+        }
+        
+        public string GetFullName(out String fullname)
+        {
+            String firstname = "Bishal";
+            String lastname = "Rai";
+            fullname = firstname + " " + lastname;
+            return fullname;
+        }
+
+        public int SumAll(params int[] numbers)
+        {
+            int sum = 0;
+            foreach (int num in numbers)
+            {
+                sum += num;
+            }
+            return sum;
+        }
+    }
+}
---
+++ Task3/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ParameterDemo pde = new ParameterDemo();
+
+            int number = 100;
+            Console.WriteLine(pde.increase(ref number));
+
+            string fullName;
+            Console.WriteLine(pde.GetFullName(out fullName));
+
+            Console.WriteLine(pde.SumAll(1, 2, 3, 4, 5));
+        }
+    }
+}
